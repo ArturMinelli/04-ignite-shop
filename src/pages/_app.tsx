a55bead70de4f64044800ff78f6251e8bb7daf57
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-10-25 12:41:40 -0300

    Commit: Estrutura visual - Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
 
+import logoImg  from '../assets/logo.svg'
+import { Container, Header, LogoContainer } from "../styles/pages/app"
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <LogoContainer>
+          <img src={logoImg.src} alt="logo" />
+          <div>
+            <strong>Ignite</strong>
+            <span>shop</span>
+          </div>
+        </LogoContainer>
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
---
+++ src/styles/pages/app.ts
@@ -0,0 +1,32 @@
+import { styled } from "..";
+
+export const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  justifyContent: 'center',
+  minHeight: '100vh',
+})
+
+export const Header = styled('header', {
+  width: '100%',
+  padding: '2rem 0',
+  maxWidth: 1180,
+  margin: '0 auto',
+})
+
+export const LogoContainer = styled('div', {
+  display: 'flex',
+  alignItems: "flex-start",
+  gap: '0.5rem',
+
+  div: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+
+  span: {
+    color: '$gray300',
+    fontSize: '0.875rem',
+  }
+})
